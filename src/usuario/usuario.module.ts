4f65644f9db55fefad2bca8386316857266d1202
Augusto Ferreira de Abreu <contato.augustoabreu@gmail.com>
2025-06-18 13:16:08 -0400

    inciando usuario

---
+++ src/usuario/usuario.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+
+import { UsuarioController } from './usuario.controller';
+import { UsuarioService } from './usuario.service';
+
+@Module({
+  imports: [],
+  controllers: [UsuarioController],
+  providers: [
+    UsuarioService,
+  ],
+  exports: [UsuarioService]
+})
+export class UsuarioModule { }
