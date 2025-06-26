5eeaa58911bde1347983ed8fcb89e9bda402e199
Augusto Ferreira de Abreu <contato.augustoabreu@gmail.com>
2025-06-26 08:27:00 -0400

    adicionando notacao de rota publica

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from './strategy/local/local.guard';
+import { Public } from './decorators/public.decorator';
 
 @Controller()
 export class AuthController {
 
+    @Public()
     @UseGuards(LocalAuthGuard)
     @Post("/login")
     async logar(@Request() req) {
